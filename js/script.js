5aa9e2a1a9eefe4f051fbb797998d2b1dc67a64a
Julian Jimenez <julian.j.jimenez@student.uncommonhs.org>
2018-03-15 22:18:23 +0000

    used for loops to get all songs on the page

--- js/script.js
+++ js/script.js
@@ -17,58 +17,78 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	title: "Overnight",
+	artist: "Logic",
+	mp3Url: "https://www.youtube.com/watch?v=SFKPktgfQdI",
+	imageUrl: "https://i0.wp.com/s1.xclusivejams.com/2018/03/01-Overnight-500x500.jpg?resize=500%2C500",
+	songLengthMinutes: 3,
+	songLengthSeconds: 37
 }
 
-var myPlayList = [
-	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	},
-	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-	},
-	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-	}
 
-]
 
 
 
 // DOCUMENT READY FUNCTION
-$( document ).ready(function() {
-  
+$(document).ready(function() {
 
+	function humanTime(x) {
+		var numMinutes = x / 60;
+		var numSeconds = numMinutes
+	}
+
+
+
+
+
+
+
+	var myPlaylist = [{
+			title: "Overnight",
+			artist: "Logic",
+			mp3Url: "https://www.youtube.com/watch?v=SFKPktgfQdI",
+			imageUrl: "./overnight.jpg",
+		},
+		{
+			title: "Pick It Up (feat.A$AP Rocky)",
+			artist: "Famous Dex",
+			mp3Url: "https://www.youtube.com/watch?v=KzPRznjok2E",
+			imageUrl: "./pickitup.png",
+		},
+		{
+			title: "After The Storm (feat. Tyler, The Creator & Bootsy",
+			artist: "Tyler, The Creator, Bootsy Collins, Kali Uchis",
+			mp3Ul: "www.youtube.com/watch?v=oskq36AaIS4",
+			imageUrl: "./afterthestorm.jpg",
+		}
+
+	]
+
+
+
+
+
+	for ( var i=0; i<myPlaylist.length; i=i+1) {
+		$("#songs").append("<h3>" + myPlaylist[i].title + "</h3>" + "<p>" + myPlaylist[i].artist + "</p>" + "<a href=" + myPlaylist[i].mp3Url + ">Play Song</a>" + "<img src="+myPlaylist[i].imageUrl+" />" + "<h1>Length in Minutes: " + myPlaylist[i].songLengthMinutes + "</h1>" + "<h1>Length in seconds: " + myPlaylist[i].songLengthSeconds + "</h1>")
+	}
 
 
 });
 
-function displayList(){
+function displayList() {
 
 
-  
+
 }
 
-function clearList(){
-  
-  
-  
+function clearList() {
+
+
+
 }
 
-function addSong(){
- 
-  
-  
+function addSong() {
+
+
+
 }
